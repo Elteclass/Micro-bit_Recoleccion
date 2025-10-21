e076a4572e1241f2dd29fa2eb5f4827ac4a5109e
Jaime Antonio Alvarez Crisóstomo <L22211519@tijuana.tecnm.mx>
2025-10-21 16:51:11 -0700

    Actualizacion: Creacion del codigo para microbit

---
+++ microbit/makecode.js
@@ -0,0 +1,32 @@
+// Al iniciar, muestra un ícono para saber que está listo
+basic.showIcon(IconNames.Heart);
+// Configura la comunicación serial
+serial.redirect(
+    SerialPin.USB_TX,
+    SerialPin.USB_RX,
+    BaudRate.BaudRate115200
+);
+
+// Bucle principal
+basic.forever(function () {
+    // 1. Obtener los datos de los sensores
+    let temperatura = input.temperature();
+    let nivelLuz = input.lightLevel();
+    let accelX = input.acceleration(Dimension.X);
+
+    // 2. Crear un objeto (diccionario) con los datos
+    let datosSensores = {
+        "temp_c": temperatura,
+        "luz": nivelLuz,
+        "accel_x": accelX
+    };
+
+    // 3. Convertir el objeto a una cadena de texto JSON
+    let jsonString = JSON.stringify(datosSensores);
+
+    // 4. Enviar la cadena JSON por el puerto serie, seguida de un salto de línea
+    serial.writeLine(jsonString);
+
+    // 5. Pausa de 1 segundo (1000 ms) antes de la siguiente lectura
+    basic.pause(1000);
+});
